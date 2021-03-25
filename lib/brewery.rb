cf2420e7af3d7d5768d65304697ea0f209cc0f0c
Valencia Caldwell <VKCaldwell@gmail.com>
2021-03-24 20:57:03 -0500

    set up cli class

---
+++ lib/brewery.rb
@@ -0,0 +1,5 @@
+class Brewery
+    #keeps track of all brewery's attributes  & saves all the breweries
+    #creates new breweries
+    # takes in :city, :state, :phone, :website_url, :street (taken from api)
+end
---
+++ lib/cli.rb
@@ -0,0 +1,10 @@
+class CLI
+    #interact with user
+    #any puts or get statements
+    #control the flow of your project 
+    def start
+        puts "Welcome"
+    end
+
+end
+
